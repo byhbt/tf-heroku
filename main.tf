cf3b08cda1036963c96e0332e6989a59d7184cb0
Thanh Huynh (Thành) <huynhbathanh@gmail.com>
2021-03-22 11:30:33 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,23 +18,23 @@ resource "heroku_app" "production" {
 
   config_vars = {
     AVAILABLE_LOCALES = "en"
-		AWS_ACCESS_KEY_ID = ""
-		AWS_S3_BUCKET_NAME = ""
-		AWS_S3_REGION = ""
-		AWS_SECRET_ACCESS_KEY = ""
-		AWS_STORAGE_ENABLED = ""
-		#DATABASE_URL = "" # attach in addons.tf
-		DEFAULT_LOCALE = "en"
-		FALLBACK_LOCALES = "en"
-		LANG = "en_US.UTF-8"
-		MAILER_DEFAULT_HOST = "localhost"
-		MAILER_DEFAULT_PORT = "3000"
-		MAILER_SENDER = "huynhbathanh@gmail.com"
-		RACK_ENV = "production"
-		RAILS_ENV = "production"
-		RAILS_LOG_TO_STDOUT = "enabled"
-		RAILS_SERVE_STATIC_FILES = "enabled"
-		SECRET_KEY_BASE = ""
+    AWS_ACCESS_KEY_ID = ""
+    AWS_S3_BUCKET_NAME = ""
+    AWS_S3_REGION = ""
+    AWS_SECRET_ACCESS_KEY = ""
+    AWS_STORAGE_ENABLED = ""
+    #DATABASE_URL = "" # attach in addons.tf
+    DEFAULT_LOCALE = "en"
+    FALLBACK_LOCALES = "en"
+    LANG = "en_US.UTF-8"
+    MAILER_DEFAULT_HOST = "localhost"
+    MAILER_DEFAULT_PORT = "3000"
+    MAILER_SENDER = "huynhbathanh@gmail.com"
+    RACK_ENV = "production"
+    RAILS_ENV = "production"
+    RAILS_LOG_TO_STDOUT = "enabled"
+    RAILS_SERVE_STATIC_FILES = "enabled"
+    SECRET_KEY_BASE = ""
   }
 
   buildpacks = var.heroku_app_buildpacks
